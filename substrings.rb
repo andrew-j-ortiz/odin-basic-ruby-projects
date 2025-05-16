f89026be5f8794e74e775f57357132140c139de7
Andrew J Ortiz <112707094+andrew-j-ortiz@users.noreply.github.com>
2025-05-16 16:34:48 -0400

    Finished substrings

--- substrings.rb
+++ substrings.rb
@@ -1,1 +1,12 @@
- 
+# Substrings identifier by Andrew Ortiz for the Odin Project
+
+DICTIONARY = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings(str_words, arr_dictionary)
+  arr_dictionary.each_with_object({}) do |dictionary_word, result|
+    int_word_instances = str_words.downcase.scan(dictionary_word).length
+    result[dictionary_word] = int_word_instances if int_word_instances > 0 
+  end
+end
+
+puts substrings("Howdy partner, sit down! How's it going?", DICTIONARY)
