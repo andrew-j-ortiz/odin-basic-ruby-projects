6c7a1577557371b2af538aa2edfa62f4cf0edc72
Andrew J Ortiz <112707094+andrew-j-ortiz@users.noreply.github.com>
2025-05-19 21:27:23 -0400

    Finished bubble sort

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,5 +1,41 @@
-NUMBERS = [4,3,78,2,0,2]
+# Bubble Sorter by Andrew Ortiz for the Odin Project
 
-def bubble_sort()
-  
+def main()
+  puts "================="
+  puts "  BUBBLE SORTER  "
+  puts "================="
+  puts
+
+  puts "Please enter your list of numbers to be sorted separated by a comma"
+  puts "e.g. 4,3,78,2,0,2"
+  print ">"
+  user_input = gets.chomp()
+  puts
+
+  arr_sorted_numbers = bubble_sort(user_input.split(',').map(&:to_i))
+  print "Your sorted numbers: " + arr_sorted_numbers.to_s
+  puts
 end
+
+def bubble_sort(arr_numbers_constant)
+  arr_numbers = Array.new + arr_numbers_constant
+
+  sorted = true
+  until sorted == false
+    array_copy = Array.new + arr_numbers
+    for index in 0..arr_numbers.length
+      unless arr_numbers[index + 1] == nil
+        if arr_numbers[index] > arr_numbers[index + 1]
+          arr_numbers[index], arr_numbers[index + 1] = arr_numbers[index + 1], arr_numbers[index]
+        end
+      end
+    end
+    if array_copy == arr_numbers
+      sorted = false
+    end
+  end
+
+  arr_numbers
+end
+
+main()
